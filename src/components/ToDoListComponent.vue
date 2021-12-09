bd97daa0c026a88193d8ce7dadeccbee29580c44
Emory Blair <95658583+Blair-Emory@users.noreply.github.com>
2021-12-08 20:09:11 -0600

    End of Session 1
    Attempted to start on dynamic insertion of task items

--- src/components/ToDoListComponent.vue
+++ src/components/ToDoListComponent.vue
@@ -3,16 +3,24 @@
 <template>
   <div>
     <QItemLabel> List Title Here </QItemLabel>
-    <QList>
+    <QList ref="list">
 
-      <to-do-item-component label= "The label goes here Emory"></to-do-item-component>
-      <to-do-item-component label= "This is a second item Bitch"></to-do-item-component>
+      <to-do-item-component label= "This is the first Item"></to-do-item-component>
+      <to-do-item-component label= "This is the second"></to-do-item-component>
 
     </QList>
+    <q-btn
+      round
+      dense
+      color="secondary"
+      icon="add"
+      @click="onClick"/>
+
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import ToDoItemComponent from './ToDoItemComponent.vue';
 
 
@@ -20,6 +28,17 @@ import ToDoItemComponent from './ToDoItemComponent.vue';
 
     name: 'ToDoList',
     components: { ToDoItemComponent },
+    methods: {
+      onClick(){
+        var ComponentClass = Vue.extend(ToDoItemComponent);
+        var instance = new ComponentClass({
+          propsData: { label: 'I was added by clicking the button below' }
+        })
+
+        instance.$mount()
+        this.$refs.list.appendChild(instance.$el);
+      }
+    }
 
   };
 </script>
