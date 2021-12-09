8014d4629440a3470100730611e3d9f3b45fb99b
Emory Blair <95658583+Blair-Emory@users.noreply.github.com>
2021-12-08 18:36:16 -0600

    Color Customization

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@
 </template>
 <script>
 import { defineComponent } from 'vue';
+import { useQuasar }from 'quasar';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+
+  setup() {
+    const $q = useQuasar();
+    $q.dark.set(true);
+  }
 })
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,9 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div>
+      Hell Yeah Motherfuckers!!
+      I can Code!
+    </div>
   </q-page>
 </template>
 
