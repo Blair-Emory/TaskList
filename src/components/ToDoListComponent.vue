39b0434b1372748e3843be3c7705755a43756020
Emory Blair <95658583+Blair-Emory@users.noreply.github.com>
2022-01-05 17:17:12 -0600

    Add task implemented
    You can now ad new tasks to the list using a dialog prompt that pops up at the push of the 'plus' button

--- src/components/ToDoListComponent.vue
+++ src/components/ToDoListComponent.vue
@@ -3,54 +3,75 @@
 
 <!-- Make the card for the list to exist on. -->
 <template>
-  <q-card flat>
+  <div>
 
-    <!--List Title-->
-    <q-card-section >
-      <div class="row items-center">
-        <div class="col">
-          <QItemLabel class="text-weight-medium"> {{title}} </QItemLabel>
+    <!-- Dialog Prompts-->
+
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">New Task</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input dense v-model="task" autofocus @keyup.enter="prompt = false; addTask(); data = '';" />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Add Task" v-close-popup @click="addTask(); data = '';"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <!-- List Card -->
+    <q-card flat>
+
+      <!--List Title-->
+      <q-card-section>
+        <div class="row items-center">
+          <div class="col">
+            <QItemLabel class="text-weight-medium"> {{title}} </QItemLabel>
+          </div>
+
+
+          <!--Options Button-->
+          <div class="col-auto">
+            <q-btn color="white" round flat dense icon="more_vert">
+              <q-menu cover auto-close>
+                <q-list>
+                  <q-item clickable>
+                    <q-item-section>Remove List</q-item-section>
+                  </q-item>
+                  <q-item clickable>
+                    <q-item-section>Edit List</q-item-section>
+                  </q-item>
+                  <q-item clickable>
+                    <q-item-section>Share</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
         </div>
+      </q-card-section>
 
+      <q-separator />
 
-        <!--Options Button-->
-        <div class="col-auto">
-         <q-btn color="white" round flat dense icon="more_vert">
-           <q-menu cover auto-close>
-             <q-list>
-               <q-item clickable>
-                  <q-item-section>Remove List</q-item-section>
-                </q-item>
-                <q-item clickable>
-                  <q-item-section>Edit List</q-item-section>
-                </q-item>
-                <q-item clickable>
-                  <q-item-section>Share</q-item-section>
-                </q-item>
-              </q-list>
-            </q-menu>
-          </q-btn>
-        </div>
-      </div>
-    </q-card-section>
+      <!-- To-Do items -->
+      <q-card-section>
+        <div>
 
-    <q-separator/>
+          <!-- This will iterate through every task in tasks list and create an item for it -->
+          <q-item v-for="(taskItem, index) in tasks"
+                  :key="taskItem.label"
+                  @click="taskItem.done = !taskItem.done"
+                  :class="{ 'done' : taskItem.done }"
+                  class="row items-center justify-between"
+                  clickable
+                  v-ripple>
 
-    <!-- To-Do items -->
-    <q-card-section>
-      <div>
-
-        <!-- This will iterate through every task in tasks list and create an item for it -->
-        <q-item
-          v-for="(taskItem, index) in tasks"
-          :key="taskItem.label"
-          @click="taskItem.done = !taskItem.done"
-          :class="{ 'done' : taskItem.done }"
-          class="row items-center justify-between"
-          clickable
-          v-ripple>
-
-          <!-- Task Item -->
+            <!-- Task Item -->
             <div class="col-auto">
               <q-item-section avatar>
                 <q-checkbox v-model="taskItem.done"
@@ -69,96 +90,64 @@
                        flat
                        dense
                        icon="delete"
-                       @click.stop="deleteTask(index)"/>
+                       @click.stop="deleteTask(index)" />
               </q-item-section>
             </div>
 
 
-        </q-item>
-      </div>
-    </q-card-section>
+          </q-item>
+        </div>
+      </q-card-section>
 
-    <!-- Add new Item button -->
-    <q-card-section>
-      <div class="col-auto">
-        <q-btn
-          round
-          dense
-          color="secondary"
-          icon="add"
-          clickable
-          @click="newTask"/>
-      </div>
-    </q-card-section>
+      <!-- Add new Item button -->
+      <q-card-section>
+        <div class="col-auto">
+          <q-btn round
+                 dense
+                 color="secondary"
+                 icon="add"
+                 clickable
+                 @click="prompt = true" />
+        </div>
+      </q-card-section>
 
-  </q-card>
+    </q-card>
+  </div>
 </template>
 
 <script>
 import ToDoItemComponent from './ToDoItemComponent.vue';
 import { useQuasar } from 'quasar';
-
+  import { ref } from 'vue';
   export default {
 
     name: 'ToDoList',
 
     data(){
       return {
+        //Dialog
+        prompt: ref(false),
+        task: ref(''),
+
+        //List
         title: 'New List',
-        tasks: [
-          {
-            label: 'Blah',
-            done: false
-          },
-          {
-            label: "Blah Blah",
-            done: false
-          },
-          {
-            label: "some really long string to see if this wraps or extends the card",
-            done: true
-          },
-          {
-            label: "Even longer label to see if this truely messes up everything that goes on within this card. Lets see how this goes. Woop Woop.",
-            done: true
-          }
-        ]
+        tasks: [ ]
       }
     },
-    setup() {
-      const $q = useQuasar()
+    
+    methods: {
+      addTask(){
 
-      function newTask () {
+        var taskValue = this.task
 
-        $q.dialog({
-          dark: true,
-          title: 'New Task',
-          prompt: {
-            model: '',
-            type: 'text'
-          },
-          cancel: true,
-          persistent: true
-        }).onOk(data => {
-
-          //Create the new tasks
+        if (taskValue != '') {
           this.tasks.push({
-            label: data,
+            label: taskValue,
             done: false
           })
 
-        }).onCancel(() => {
-          //Cancel creating new task
-        }).onDismiss(() => {
-          //Triggered on both Okay and Cancelled
-        })
-      }
-
-      return { newTask }
-    },
-    methods: {
-      addTask(data){
-
+          this.task = ''
+        }
 
       },
 
